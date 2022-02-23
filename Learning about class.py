6ee4c4fc42aa390fc14dac4c325895d65f0c3648
Eshal19 <98449428+Eshal19@users.noreply.github.com>
2022-02-23 17:49:17 +0530

    Add files via upload

---
+++ Learning about class.py
@@ -0,0 +1,16 @@
+class Cars:
+    colour="Purple"
+    type="camper van"
+    speed="fast"
+
+    def car(self):
+        print("i am a",self.colour,"coloured car")
+        print("i am a",self.type)
+        print("i am",self.speed)
+
+
+c=Cars()
+
+c.car()
+
+    
